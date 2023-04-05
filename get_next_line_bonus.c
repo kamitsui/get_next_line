fe66b02acc3311793ec92a8bfb8bebb788641961
Kaishiro Mitsui <kamitsui@student.42tokyo.jp>
2023-04-05 19:30:06 +0900

    Check tester OK (Tripouille bonus)

---
+++ get_next_line_bonus.c
@@ -0,0 +1,117 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kamitsui <kamitsui@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/04 14:46:05 by kamitsui          #+#    #+#             */
+/*   Updated: 2023/04/05 18:32:49 by kamitsui         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strnlen(const char *s, size_t maxlen)
+{
+	size_t	len;
+
+	len = 0;
+	if (!maxlen)
+		return (len);
+	while (*s)
+	{
+		if (len == maxlen)
+			break ;
+		len++;
+		s++;
+	}
+	return (len);
+}
+
+char	*ft_strndup(const char *s1, size_t n)
+{
+	size_t	len;
+	char	*result;
+
+	len = ft_strnlen(s1, n);
+	result = malloc(sizeof(char) * (len + 1));
+	if (!result)
+		return (NULL);
+	result[len] = '\0';
+	return (ft_memmove(result, s1, len));
+}
+
+char	*ft_newline(char **saved, int fd)
+{
+	char	*temp;
+	char	*line;
+	int		len;
+
+	if (saved[fd] == NULL)
+		return (NULL);
+	temp = ft_strchr(saved[fd], '\n');
+	if (temp != NULL)
+	{
+		len = temp - saved[fd] + 1;
+		line = ft_strndup(saved[fd], len);
+		temp = ft_strdup(temp + 1);
+		free(saved[fd]);
+		saved[fd] = temp;
+	}
+	else
+	{
+		if (!(*saved[fd] == '\0'))
+			line = ft_strdup(saved[fd]);
+		else
+			line = NULL;
+		free(saved[fd]);
+		saved[fd] = NULL;
+	}
+	return (line);
+}
+
+ssize_t	search_newline(char **saved, char *buffer, ssize_t bytes_read, int fd)
+{
+	char	*str_temp;
+	ssize_t	bytes_temp;
+
+	while (bytes_read > 0)
+	{
+		buffer[bytes_read] = '\0';
+		if (saved[fd] == NULL)
+			saved[fd] = ft_strdup(buffer);
+		else
+		{
+			str_temp = ft_strjoin(saved[fd], buffer);
+			free(saved[fd]);
+			saved[fd] = str_temp;
+		}
+		if (ft_strchr(buffer, '\n'))
+			break ;
+		bytes_temp = read(fd, buffer, BUFFER_SIZE);
+		if (bytes_temp < 0)
+			return (bytes_temp);
+		bytes_read = bytes_temp;
+	}
+	return (bytes_read);
+}
+
+char	*get_next_line(int fd)
+{
+	char		buffer[BUFFER_SIZE + 1];
+	ssize_t		bytes_read;
+	static char	*saved[256];
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	bytes_read = read(fd, buffer, BUFFER_SIZE);
+	if (bytes_read < 0)
+		return (NULL);
+	bytes_read = search_newline(saved, buffer, bytes_read, fd);
+	if (bytes_read == -1)
+		return (NULL);
+	if (bytes_read == 0 && saved[fd] == NULL)
+		return (NULL);
+	return (ft_newline(saved, fd));
+}
---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,97 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kamitsui <kamitsui@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/04 15:31:14 by kamitsui          #+#    #+#             */
+/*   Updated: 2023/04/04 15:45:35 by kamitsui         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	len;
+
+	len = 0;
+	while (*(s++))
+		len++;
+	return (len);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	char	ch;
+
+	ch = (char)c;
+	while (1)
+	{
+		if (*s == ch)
+			return ((char *)s);
+		if (*s == '\0')
+			break ;
+		s++;
+	}
+	return ((char *)0);
+}
+
+void	*ft_memmove(void *dst, const void *src, size_t n)
+{
+	unsigned char	*to;
+	unsigned char	*from;
+	int				d;
+
+	to = (unsigned char *)dst;
+	from = (unsigned char *)src;
+	if (dst == src || n == 0)
+		return (dst);
+	if (to > from)
+	{
+		to += n - 1;
+		from += n - 1;
+		d = -1;
+	}
+	else
+		d = +1;
+	while (n-- > 0)
+	{
+		*(unsigned char *)to = *(unsigned char *)from;
+		to += d;
+		from += d;
+	}
+	return (dst);
+}
+
+char	*ft_strdup(const char *s)
+{
+	size_t	len;
+	char	*str;
+
+	len = ft_strlen(s);
+	str = malloc(sizeof(char) * (len + 1));
+	if (str == NULL)
+		return (NULL);
+	str[len] = '\0';
+	return (ft_memmove(str, s, len));
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*joined;
+	size_t	len1;
+	size_t	len2;
+
+	if ((s1 == NULL) || (s2 == NULL))
+		return (NULL);
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	joined = (char *)malloc(sizeof(char) * (len1 + len2 + 1));
+	if (joined == NULL)
+		return (NULL);
+	ft_memmove(joined, s1, len1);
+	ft_memmove(joined + len1, s2, len2 + 1);
+	return (joined);
+}
