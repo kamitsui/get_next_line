e6d36b9d3fd50d8e1d2680bd13df2f44c92b0093
Kaishiro Mitsui <kamitsui@student.42tokyo.jp>
2023-04-05 14:12:33 +0900

    Fix abort (malloc failed)

--- get_next_line.c
+++ get_next_line.c
@@ -6,12 +6,11 @@
 /*   By: kamitsui <kamitsui@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 14:46:05 by kamitsui          #+#    #+#             */
-/*   Updated: 2023/04/05 13:42:04 by kamitsui         ###   ########.fr       */
+/*   Updated: 2023/04/05 14:11:13 by kamitsui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <string.h>//for check
 
 size_t	ft_strnlen(const char *s, size_t maxlen)
 {
@@ -43,28 +42,13 @@ char	*ft_strndup(const char *s1, size_t n)
 	return (ft_memmove(result, s1, len));
 }
 
-//char	*ft_substrline(char *str, char *str_nl)
-//{
-//	char	*line;
-//	size_t	len;
-//
-//	if (str == NULL)
-//		return (NULL);
-//	len = str_nl - str;
-//	line = ft_strndup(str, len);
-//	if (line == NULL)
-//		return (NULL);
-//	str_nl = ft_strdup(str_nl + 1);
-//	saved[fd] = ;
-//}
-
 char	*ft_newline(char **saved, int fd, int bytes_read)
 {
 	char	*temp;
 	char	*line;
 	int		len;
 
-	if ((saved[fd] == NULL) && (bytes_read == 0))
+	if ((saved[fd] == NULL) || (bytes_read == 0))
 		return (NULL);
 	temp = ft_strchr(saved[fd], '\n');
 	if (temp != NULL)
@@ -84,10 +68,10 @@ char	*ft_newline(char **saved, int fd, int bytes_read)
 	return (line);
 }
 
-ssize_t	seach_nl(char **saved, char *buffer, ssize_t bytes_read, int fd)
+ssize_t	search_newline(char **saved, char *buffer, ssize_t bytes_read, int fd)
 {
-	char	*str_tmp;
-	ssize_t	bytes_tmp;
+	char	*str_temp;
+	ssize_t	bytes_temp;
 
 	while (bytes_read > 0)
 	{
@@ -96,17 +80,17 @@ ssize_t	seach_nl(char **saved, char *buffer, ssize_t bytes_read, int fd)
 			saved[fd] = ft_strdup(buffer);
 		else
 		{
-			str_tmp = ft_strjoin(*saved, buffer);
+			str_temp = ft_strjoin(*saved, buffer);
 			free(saved[fd]);
-			saved[fd] = str_tmp;
+			saved[fd] = str_temp;
 		}
 		if (ft_strchr(buffer, '\n'))
 			break ;
-		bytes_tmp = read(fd, buffer, BUFFER_SIZE);
-		if (bytes_tmp < 0)
-			return (bytes_tmp);
+		bytes_temp = read(fd, buffer, BUFFER_SIZE);
+		if (bytes_temp < 0)
+			return (bytes_temp);
 		else
-			bytes_read = bytes_tmp;
+			bytes_read = bytes_temp;
 	}
 	return (bytes_read);
 }
@@ -122,29 +106,10 @@ char	*get_next_line(int fd)
 	bytes_read = read(fd, buffer, BUFFER_SIZE);
 	if (bytes_read < 0)
 		return (NULL);
-	bytes_read = seach_nl(saved, buffer, bytes_read, fd);
+	bytes_read = search_newline(saved, buffer, bytes_read, fd);
 	if (bytes_read == -1)
 		return (NULL);
 	if (bytes_read == 0 && saved[fd] == NULL)
 		return (NULL);
 	return (ft_newline(saved, fd, bytes_read));
 }
-//void code
-//	char		buffer[BUFFER_SIZE + 1];
-//	char		*tmp;
-//	bytes_read = read(fd, buffer, BUFFER_SIZE);
-//	while (bytes_read > 0)
-//	{
-//		buffer[bytes_read] = '\0';
-//		if (saved[fd] == NULL)
-//			saved[fd] = ft_strdup(buffer);
-//		else
-//		{
-//			tmp = ft_strdup(buffer);
-//			free(saved[fd]);
-//			saved[fd] = tmp;
-//		}
-//		if (ft_strchr(buffer, '\n'))
-//			break ;
-//		bytes_read = read(fd, buffer, BUFFER_SIZE);
-//	}
